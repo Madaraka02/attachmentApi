8030e73194ebd1d32ccaf999634d84c60b10867d
Madaraka02 <victormadaraka02@gmail.com>
2021-08-20 00:28:45 +0300

    added whitenoise

--- school_Api/settings.py
+++ school_Api/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
