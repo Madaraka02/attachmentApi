60c63ee395f43c5d9ce7e463c4209c917192ef55
Madaraka02 <victormadaraka02@gmail.com>
2021-09-03 13:34:16 +0300

    updated all endpoint naming

--- api/urls.py
+++ api/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('api/v1/schools/add/', schoolCreate, name='school-create'),
     path('api/v1/schools/', schoolList, name='schools'),
     path('api/v1/schools/<str:id>/', schoolDetail, name='school-detail'),
-    path('api/v1/schools/update/<str:id>/', schoolUpdate, name='school-update'),
-    path('api/v1/schools/delete/<str:id>/', schoolDelete, name='school-delete'),
+    path('api/v1/schools/<str:id>/update/', schoolUpdate, name='school-update'),
+    path('api/v1/schools/<str:id>/delete/', schoolDelete, name='school-delete'),
 
     # student api
 
@@ -20,14 +20,14 @@ urlpatterns = [
     path('api/v2/students/', studentList, name='students'),
     path('api/v2/students/<str:id>/', studentDetail, name='student-detail'),
     path('api/v2/students/school/<str:slug>/', studentSchoolDetail, name='student-school-detail'),
-    path('api/v2/students/update/<str:id>/', studentUpdate, name='student-update'),
-    path('api/v2/students/delete/<str:id>/', studentDelete, name='student-delete'),
+    path('api/v2/students/<str:id>/update/', studentUpdate, name='student-update'),
+    path('api/v2/students/<str:id>/delete/', studentDelete, name='student-delete'),
 
     # company api
 
     path('api/v3/companies/add/', companyCreate, name='company-create'),
     path('api/v3/companies/', companyList, name='companyies'),
     path('api/v3/companies/<str:id>/', companyDetail, name='company-detail'),
-    path('api/v3/companies/update/<str:id>/', companyUpdate, name='company-update'),
-    path('api/v3/companies/delete/<str:id>/', companyDelete, name='company-delete'),
+    path('api/v3/companies/<str:id>/update/', companyUpdate, name='company-update'),
+    path('api/v3/companies/<str:id>/delete/', companyDelete, name='company-delete'),
 ]
