7d402acdf63b2a33b421e4e15b0132cb171d58ec
Madaraka02 <victormadaraka02@gmail.com>
2021-09-08 08:43:54 +0300

    added rest-auth

--- school_Api/urls.py
+++ school_Api/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rest-auth/', include('rest_auth.urls'))
+    path('rest-auth/', include('rest_auth.urls')),
     path('', include('api.urls'))
 ]
 
