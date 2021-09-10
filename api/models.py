aed2d1b7fde5d11f1bd73028f54c1a3f7c74d9bb
Madaraka02 <victormadaraka02@gmail.com>
2021-09-10 17:44:27 +0300

    ...

--- api/models.py
+++ api/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+
 # Create your models here.
 
 class School(models.Model):
@@ -15,24 +13,12 @@ class School(models.Model):
         ordering = ('-id',)    
 
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=1000)
     reg_no = models.CharField(max_length=100)
     course = models.CharField(max_length=100)
     transcript = models.FileField()
     year_of_completion = models.CharField(max_length=100)
     campus = models.ForeignKey(School, on_delete=models.CASCADE, related_name='students')
-
-    @receiver(post_save, sender=User)
-    def create_user_student(sender, instance, created, **kwargs):
-        if created:
-            Student.objects.create(user=instance)
-
-    @receiver(post_save, sender=User)
-    def save_user_student(sender, instance, **kwargs):
-        instance.student.save()
-
-    
 
     def __str__(self):
         return self.reg_no
@@ -63,8 +49,3 @@ class Jobs(models.Model):
 
     class Meta:
         ordering = ('-id',)    
-
-class RegisterStudents(models.Model):
-    username = models.CharField(max_length=)  
-    password = 
-    student =      
