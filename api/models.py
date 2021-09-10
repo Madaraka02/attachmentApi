6f6d5818f2815e4324b7e0378a358cb569c7c919
Madaraka02 <victormadaraka02@gmail.com>
2021-09-10 17:09:16 +0300

    added authentication backends

--- api/models.py
+++ api/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
 class School(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=2000)
     slug = models.SlugField()
 
@@ -13,12 +17,22 @@ class School(models.Model):
         ordering = ('-id',)    
 
 class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=1000)
     reg_no = models.CharField(max_length=100)
     course = models.CharField(max_length=100)
     transcript = models.FileField()
     year_of_completion = models.CharField(max_length=100)
     campus = models.ForeignKey(School, on_delete=models.CASCADE, related_name='students')
+
+    @receiver(post_save, sender=User)
+    def create_user_student(sender, instance, created, **kwargs):
+        if created:
+            Student.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_student(sender, instance, **kwargs):
+        instance.student.save()
 
     def __str__(self):
         return self.reg_no
@@ -27,6 +41,7 @@ class Student(models.Model):
         ordering = ('-id',) 
 
 class Company(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=2000)
     reg_number = models.CharField(max_length=3000)
     slug = models.SlugField()
