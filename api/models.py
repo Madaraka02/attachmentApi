7c813ee7839a53bb0311690075c8da316a0b3018
Madaraka02 <victormadaraka02@gmail.com>
2021-09-10 18:25:25 +0300

    added passwordfield

--- api/models.py
+++ api/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 
 class School(models.Model):
+    def school():
+        return 'school'
     name = models.CharField(max_length=2000)
-    password = models.CharField(max_length=50, null=False, blank=False, default='school')
+    password = models.CharField(max_length=50, null=False, blank=False, default=school)
     slug = models.SlugField()
+
 
     def __str__(self):
         return self.name
@@ -14,13 +17,17 @@ class School(models.Model):
         ordering = ('-id',)    
 
 class Student(models.Model):
+
+    def student():
+        return 'password'
     name = models.CharField(max_length=1000)
     reg_no = models.CharField(max_length=100)
-    password = models.CharField(max_length=50, null=False, blank=False, default='password')
+    password = models.CharField(max_length=50, null=False, blank=False, default=student)
     course = models.CharField(max_length=100)
     transcript = models.FileField()
     year_of_completion = models.CharField(max_length=100)
     campus = models.ForeignKey(School, on_delete=models.CASCADE, related_name='students')
+
 
     def __str__(self):
         return self.reg_no
@@ -29,10 +36,14 @@ class Student(models.Model):
         ordering = ('-id',) 
 
 class Company(models.Model):
+    
+    def company():
+        return 'company'
     name = models.CharField(max_length=2000)
-    password = models.CharField(max_length=50, null=False, blank=False, default='company')
+    password = models.CharField(max_length=50, null=False, blank=False, default=company)
     reg_number = models.CharField(max_length=3000)
     slug = models.SlugField()
+
 
     def __str__(self):
         return self.name
