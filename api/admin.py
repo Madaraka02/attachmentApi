27d9fc156a017d8238135bc5fcea3f81edaad131
Madaraka02 <victormadaraka02@gmail.com>
2021-09-10 18:41:14 +0300

    updated urls for jobs

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import School, Student
+from .models import School, Student, Company, Jobs
 
 # Register your models here.
 
 admin.site.register(School)
 admin.site.register(Student)
+admin.site.register(Company)
+admin.site.register(Jobs)
